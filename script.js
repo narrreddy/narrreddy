0316f2433e6cb3052ab81ae7e7ac0fbfab44c0ab
narrreddy <63010034+narrreddy@users.noreply.github.com>
2025-08-09 08:36:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-function scrollToNextSection(element) {
-  const sections = document.querySelectorAll("section");
-  const currentSection = element.closest("section");
-  let nextSection = null;
-  for (let i = 0; i < sections.length; i++) {
-    if (sections[i] === currentSection && i < sections.length - 1) {
-      nextSection = sections[i + 1];
-      break;
-    }
-  }
-  if (nextSection) {
-    nextSection.scrollIntoView({ behavior: "smooth" });
-  }
-}
-
-function scrollToSection(id) {
-  const section = document.getElementById(id);
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
-}
